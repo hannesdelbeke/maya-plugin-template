dcf1839a7b6eb1d2410010d311c52e5ddbc5e986
Hannes Delbeke <hannesdelbeke@gmail.com>
2023-10-23 17:56:48 +0100

    fix menu

--- my_plugin.py
+++ my_plugin.py
@@ -53,20 +53,21 @@ def show(*args):
 
 
 def loadMenu():
-    if not cmds.menu(MENU_NAME, exists=True):
+    if not cmds.menu(f"{MENU_PARENT}|{MENU_NAME}", exists=True):
         cmds.menu(MENU_NAME, label=MENU_LABEL, parent=MENU_PARENT)
-    cmds.menuItem(label=MENU_ENTRY_LABEL, command=show)  # , parent=MENU_NAME)  
+    cmds.menuItem(label=MENU_ENTRY_LABEL, command=show, parent=MENU_NAME)  
 
 
 def unloadMenuItem():
-    if cmds.menu(MENU_NAME, exists=True):
-        menu_item_long_name = MENU_NAME + "|" + MENU_ENTRY_LABEL
+    if cmds.menu(f"{MENU_PARENT}|{MENU_NAME}", exists=True):
+        menu_long_name = f"{MENU_PARENT}|{MENU_NAME}"
+        menu_item_long_name = f"{menu_long_name}|{MENU_ENTRY_LABEL}"
         # Check if the menu item exists; if it does, delete it
         if cmds.menuItem(menu_item_long_name, exists=True):
             cmds.deleteUI(menu_item_long_name, menuItem=True)
         # Check if the menu is now empty; if it is, delete the menu
-        if not cmds.menu(MENU_NAME, query=True, itemArray=True):
-            cmds.deleteUI(MENU_NAME, menu=True)
+        if not cmds.menu(menu_long_name, query=True, itemArray=True):
+            cmds.deleteUI(menu_long_name, menu=True)
 
 
 # =============================== Plugin (un)load ===========================================
