460ea0b347ba075a939db68b1cc2a1f06feb6c7c
ElgoogUdiab <ElgoogUdiab@users.noreply.github.com>
2024-01-11 22:18:06 +0800

    typo fix

--- my_plugin.py
+++ my_plugin.py
@@ -9,7 +9,7 @@ MENU_ENTRY_LABEL = "My cool tool"
 
 MENU_PARENT = "MayaWindow"  # do not change
 
-__meun_entry_name = "" # Store generated menu item, used when unregister
+__menu_entry_name = "" # Store generated menu item, used when unregister
 
 def maya_useNewAPI():  # noqa
     pass  # dummy method to tell Maya this plugin uses Maya Python API 2.0
@@ -55,18 +55,18 @@ def show(*args):
 
 
 def loadMenu():
-    global __meun_entry_name
+    global __menu_entry_name
     if not cmds.menu(f"{MENU_PARENT}|{MENU_NAME}", exists=True):
         cmds.menu(MENU_NAME, label=MENU_LABEL, parent=MENU_PARENT)
-    __meun_entry_name = cmds.menuItem(label=MENU_ENTRY_LABEL, command=show, parent=MENU_NAME)
+    __menu_entry_name = cmds.menuItem(label=MENU_ENTRY_LABEL, command=show, parent=MENU_NAME)
 
 
 def unloadMenuItem():
     if cmds.menu(f"{MENU_PARENT}|{MENU_NAME}", exists=True):
         menu_long_name = f"{MENU_PARENT}|{MENU_NAME}"
         # Check if the menu item exists; if it does, delete it
-        if cmds.menuItem(__meun_entry_name, exists=True):
-            cmds.deleteUI(__meun_entry_name, menuItem=True)
+        if cmds.menuItem(__menu_entry_name, exists=True):
+            cmds.deleteUI(__menu_entry_name, menuItem=True)
         # Check if the menu is now empty; if it is, delete the menu
         if not cmds.menu(menu_long_name, query=True, itemArray=True):
             cmds.deleteUI(menu_long_name, menu=True)
